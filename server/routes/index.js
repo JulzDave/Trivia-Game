6bae7e3b5f2c51c2b81bd6f7e03a5ae0de0e9750
Julian <juliandave1989@gmail.com>
2019-07-21 10:12:31 +0300

    fixed responsive design

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,16 @@ var express = require('express');
 var router = express.Router();
 var fs = require("fs");
 
-router.get('/read', function(req, res, next) {
+// router.get('/read', function(req, res, next) {
+// let dataJson = fs.readFileSync('data.json', 'utf8')
+// res.json(JSON.parse(dataJson))
+// });
 
-let dataJson = fs.readFileSync('data.json', 'utf8')
-res.json(JSON.parse(dataJson))
+router.get('/read', function (req, res, next) {
+
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    res.json(JSON.parse(data))
+  });
 
 });
 
